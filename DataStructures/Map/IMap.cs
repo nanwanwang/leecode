87d6f4c7dbc1234188e75aad7decb56d42588124
demonwang <nanwanwang@hotmail.com>
2021-10-21 10:31:56 +0800

    demon#2021021_map&set interface define

---
+++ DataStructures/Map/IMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Map
+{
+    public interface IMap<K,V>
+    {
+        int Size();
+        bool IsEmpty();
+
+        void Clear();
+
+        V Put(K key, V value);
+
+        V Get(K key);
+
+        V Remove(K key);
+
+        bool ContainsKey(K key);
+
+        bool ContainsValue(V value);
+
+        void Traversal(Action<K, V> action);
+    }
+}
---
+++ DataStructures/Set/ISet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Set
+{
+    public interface ISet<T>
+    {
+        int Size();
+
+        bool IsEmpty();
+
+        void Clear();
+
+        bool Contains();
+
+        void Add(T element);
+
+        void Remove(T element);
+
+
+        /// <summary>
+        /// 遍历
+        /// </summary>
+        /// <param name="action"></param>
+        void Traversal(Action<T> action);
+
+        
+    }
+}
