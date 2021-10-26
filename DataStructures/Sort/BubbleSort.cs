5ce0a089e6371bfadab16647418b3046fc3bef46
demonwang <nanwanwang@hotmail.com>
2021-10-26 16:51:29 +0800

    add sort optimization

--- DataStructures/Sort/BubbleSort.cs
+++ DataStructures/Sort/BubbleSort.cs
@@ -6,14 +6,18 @@ using System.Threading.Tasks;
 
 namespace Sort
 {
+    /// bubble sort 最差时间复杂度  O(n^2) 最好时间复杂度O(n)
     public class BubbleSort
     {
+        ///
+        /// 普通冒泡
+        ///
         public static void Sort(List<int> list)
         {
+            int n = 0;
 
             for (int i = 0; i < list.Count; i++)
             {
-
                 for (int j = 0; j < list.Count - i - 1; j++)
                 {
                     if (list[j] > list[j + 1])
@@ -21,14 +25,69 @@ namespace Sort
                         var temp = list[j];
                         list[j] = list[j + 1];
                         list[j + 1] = temp;
+
                     }
+                    n++;
                 }
             }
 
-
-
             list.ForEach(x => Console.WriteLine(x));
+            System.Console.WriteLine($"Sort比较了{n}次");
+        }
 
+        /// 如果数据本来就是有序的 就直接跳出循环
+        public static void SortOptimization(List<int> list)
+        {
+            int n = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                var sorted = true;
+                for (int j = 0; j < list.Count - 1 - i; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        var temp = list[j + 1];
+                        list[j + 1] = list[j];
+                        list[j] = temp;
+                        sorted = false;
+                    }
+
+                    n++;
+                }
+              
+                if (sorted) break;
+            }
+
+            list.ForEach(x => System.Console.WriteLine(x));
+            System.Console.WriteLine($"SortOptimization 比较了{n}次");
+        }
+
+
+        ///局部已经有序就不需要再做交换判断
+        public static void SortOptimization2(List<int> list)
+        {
+            int n =0;
+            for (int end = list.Count - 1; end > 0; end--)
+            {
+                int sortedIndex = 1;
+                for (int start = 1; start <= end; start++)
+                {
+                    if(list[start-1]>list[start])
+                    {
+                        var temp = list[start-1];
+                        list[start-1] = list[start];
+                        list[start] = temp;
+                        sortedIndex = start;
+                    }
+                    n++;
+                }
+
+                end = sortedIndex;
+
+            }
+
+            list.ForEach(x => System.Console.WriteLine(x));
+             System.Console.WriteLine($"SortOptimization2 比较了{n}次");
         }
     }
 }
--- DataStructures/Sort/Program.cs
+++ DataStructures/Sort/Program.cs
@@ -8,8 +8,13 @@ namespace Sort
         static void Main(string[] args)
         {
 
-            var list = new List<int> { 1, 45, 54, 17, 89 };
+            var list = new List<int> { 1,8,9,4, 3, 5, 7, 9 };
+            var list2 = new List<int> { 1,8,9,4, 3, 5, 7, 9 };
+            var list3 = new List<int> { 1,8,9,4, 3, 5, 7, 9 };
             BubbleSort.Sort(list);
+            BubbleSort.SortOptimization(list2);
+            BubbleSort.SortOptimization2(list3);
+           
         }
     }
 }
